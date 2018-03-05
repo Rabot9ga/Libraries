3b5eef137f9984c80cf5adf87021a9d18e442e8c
Кленин Даниил <sbt-klenin-da@mail.ca.sbrf.ru>
2018-03-05 09:45:57 +0300

    add Concurrent Util

---
+++ concurrent_util/src/main/java/ConcurrentUtil.java
@@ -0,0 +1,27 @@
+import java.util.concurrent.ConcurrentMap;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class ConcurrentUtil {
+
+    public static <K, V> V putInMap(ConcurrentMap<K, V> map, K key, Supplier<V> valueSupplier) {
+        return putInMap(map, key, valueSupplier, null);
+    }
+
+    public static <K, V> V putInMap(ConcurrentMap<K, V> map, K key, Supplier<V> valueSupplier, Consumer<V> actionOnPut) {
+        V returnValue = map.get(key);
+        if (returnValue == null) {
+            V value = valueSupplier.get();
+            V ifAbsent = map.putIfAbsent(key, value);
+            if (ifAbsent == null) {
+                returnValue = value;
+                if (actionOnPut != null) {
+                    actionOnPut.accept(returnValue);
+                }
+            } else {
+                returnValue = ifAbsent;
+            }
+        }
+        return returnValue;
+    }
+}
